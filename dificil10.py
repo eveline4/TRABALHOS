f72f1db6ccb78478871e6e04493b600d2b40bb4f
eveline4 <54454528+eveline4@users.noreply.github.com>
2019-09-01 20:40:21 -0300

    Add files via upload

---
+++ dificil10.py
@@ -0,0 +1,12 @@
+n1 =float(input('digite um número: '))
+n2 =float(input('digite outro número: '))
+if(n1 > n2):
+    print('o número maior é', n1)
+if(n1 < n2):
+    print('o número menor é', n1)
+if(n2 > n1):
+    print('o número maior é', n2)
+if(n2 < n1):
+    print('o número menor é', n2)
+if(n1 == n2):
+    print('os números são iguais')
